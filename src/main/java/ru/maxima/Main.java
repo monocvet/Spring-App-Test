864fd147612ffcec284ff74b02c035975707b28b
monocvet <monocvet@mail.ru>
2024-05-05 01:30:27 +0300

    DZ-17 Radio

---
+++ src/main/java/ru/maxima/Main.java
@@ -0,0 +1,23 @@
+package ru.maxima;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext(
+                        "applicationcontext.xml"
+                );
+        Radio radio = context.getBean("idOfRadioMaximum", RadioMaximum.class);
+        RadioPlayer player = new RadioPlayer(radio);
+        player.playRadio();
+
+        radio = context.getBean("idOfRadioEnergy", RadioEnergy.class);
+        player = new RadioPlayer(radio);
+        player.playRadio();
+
+        radio = context.getBean("idOfRadioEuropaPlus", RadioEuropaPlus.class);
+        player = new RadioPlayer(radio);
+        player.playRadio();
+    }
+}
---
+++ src/main/java/ru/maxima/RadioPlayer.java
@@ -0,0 +1,68 @@
+package ru.maxima;
+
+interface Radio {
+    String getCurrentSong();
+}
+
+class RadioMaximum implements Radio {
+    private String song1;
+    private String song2;
+    private String song3;
+
+    public RadioMaximum(String song1, String song3, String song2) {
+        this.song1 = song1;
+        this.song3 = song3;
+        this.song2 = song2;
+    }
+
+    @Override
+    public String getCurrentSong() {
+        return song1 + " , " + song2 + " , " + song3;
+    }
+}
+
+class RadioEnergy implements Radio {
+    private String song1;
+    private String song2;
+    private String song3;
+
+    public RadioEnergy(String song1, String song3, String song2) {
+        this.song1 = song1;
+        this.song3 = song3;
+        this.song2 = song2;
+    }
+
+    @Override
+    public String getCurrentSong() {
+        return song1 + " , " + song2 + " , " + song3;
+    }
+}
+
+class RadioEuropaPlus implements Radio {
+    private String song1;
+    private String song2;
+    private String song3;
+
+    public RadioEuropaPlus(String song1, String song3, String song2) {
+        this.song1 = song1;
+        this.song3 = song3;
+        this.song2 = song2;
+    }
+
+    @Override
+    public String getCurrentSong() {
+        return song1 + " , " + song2 + " , " + song3;
+    }
+}
+
+public class RadioPlayer {
+    private Radio radio;
+
+    public RadioPlayer(Radio radio) {
+        this.radio = radio;
+    }
+
+    public void playRadio() {
+        System.out.println(radio.getCurrentSong());
+    }
+}
